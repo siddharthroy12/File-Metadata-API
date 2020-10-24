3174697636aaa374c448915e082b9899b08a083a
Siddharth Roy <siddharthroy36912@gmail.com>
2020-10-24 07:54:09 +0530

    Finished Project

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 var express = require('express');
 var cors = require('cors');
-
 // require and use "multer"...
+
+var multer  = require('multer')
 
 var app = express();
 
@@ -13,6 +14,10 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post("/api/fileanalyse", multer().single('upfile'), (req, res) => {
+  res.json({name:req.file.originalname, type:req.file.mimetype, size:req.file.size});
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
